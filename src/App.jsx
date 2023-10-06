817733dde26309eac0bd7e20ce84819098691aee
omagdy7 <omar.professional8777@gmail.com>
2023-10-06 19:46:19 +0300

    Added the logic for handling backspace

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
 import { useState, useEffect, useRef } from 'react'
 import WordCard from './components/WordCard'
-import typeclick from '../assets/mixkit-typewriter-hard-hit-1367.wav'
-import returnClick from '../assets/mixkit-typewriter-classic-return-1381.wav'
-import bgMusic from '../assets/background_music.mp3'
-import dictonary from "../assets/words.json"
+import typeclick from './assets/mixkit-typewriter-hard-hit-1367.wav'
+import returnClick from './assets/mixkit-typewriter-classic-return-1381.wav'
+import bgMusic from './assets/background_music.mp3'
+import dictonary from "./assets/words.json"
 
 function App() {
+  // Constants
   const listOfWords = dictonary["listOfWords"]
-  const maxHeight = window.innerHeight
-  const spawnTime = 2000
   const rightCharColor = "#0284c7"
   const shadowBoxColor = "#06b6d4"
+  const normalCharColor = "rgba(139,69,19,0.5)"
+  const maxHeight = window.innerHeight
   const minX = 200
-  const maxX = 1700
-
+  const maxX = 1600
+  const spawnTime = 2000
   const click = new Audio(typeclick)
   const finishClick = new Audio(returnClick)
 
+  // States
   const [words, setWords] = useState([{ word: listOfWords[getRandomInt(0, listOfWords.length - 1)]["word"], posX: getRandomInt(minX, maxX) + "px" }])
   const [curIdx, setCurIdx] = useState(0);
   const [curWord, setCurWord] = useState(null)
   const [isSet, setIsSet] = useState(true)
   const [isGameOver, setIsGameOver] = useState(false)
+  const wordsRef = useRef(null)
 
+  // Helper function
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  const wordsRef = useRef(null)
 
+  // play background music
   useEffect(() => {
     const gameLoopMusic = new Audio(bgMusic)
     gameLoopMusic.loop = true;
@@ -42,7 +46,9 @@ function App() {
     }
   }, [])
 
+  // handle spawning and checks if game is over
   useEffect(() => {
+    // spawn a word after spawnTime
     const intervalId = setInterval(() => {
       setWords((prevWords) => {
         const randIdx = getRandomInt(0, listOfWords.length - 1);
@@ -54,44 +60,56 @@ function App() {
       })
     }, spawnTime)
 
+    // Checks if the game is over duh...
     const checkGameOver = setInterval(() => {
       if (wordsRef.current) {
         const currentRef = wordsRef.current;
         const children = [...currentRef.children];
         const closestElement = children.reduce((highest, current) => {
           return current.top > highest.top ? current : highest;
-        }, children[0])
+        }, children[0]) // get the closese element from the bottom
         if (closestElement) {
           const topValue = parseInt(closestElement.style.top, 10)
-          if (topValue > maxHeight - 48) {
+          if (topValue > maxHeight - 48) { // 48 is hardcoded which is bad figure out how to make it depends of the window size
             setIsGameOver(true)
           }
         }
       }
     }, 200)
 
-
+    // clean up
     return () => {
       clearInterval(intervalId)
       clearInterval(checkGameOver)
     };
   }, []);
 
-
+  // handle keypresses
   useEffect(() => {
-
     const handleKeyPress = (event) => {
       const key = event.key;
       click.play()
-      console.log(curIdx)
+      if (key === 'Backspace') {
+        if (curWord) {
+          setCurIdx((idx) => Math.max(0, idx - 1))
+          if (curIdx - 1 === 0) {
+            setIsSet(true)
+            setCurWord(null)
+            curWord.style.boxShadow = "none"
+            curWord.style.borderColor = "black"
+          }
+          const curSpan = curWord.children[0].children[curIdx - 1]
+          curSpan.style.color = normalCharColor
+        }
+      }
       if (wordsRef.current) {
         if (isSet) {
           const currentRef = wordsRef.current;
           const children = [...currentRef.children];
-          const validElements = children.filter((item) => item.children[0].children[0].innerText === key && !item.classList.contains('animate-fade'))
+          const validElements = children.filter((item) => item.children[0].children[0].innerText === key && !item.classList.contains('animate-fade')) // filter the current words which match key
           const closestElement = validElements.reduce((highest, current) => {
             return current.top > highest.top ? current : highest;
-          }, validElements[0])
+          }, validElements[0]) // get closeest element from bottom
           setCurWord(closestElement);
           if (closestElement) {
             const curSpan = closestElement.children[0].children[0]
